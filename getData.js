d12db50833e4ec0db0c03132c640cdd3b32b2ad3
Richard Bolger <bolgerr@ad.dcu.ie>
2018-04-06 14:41:13 +0100

    new file:   bookimport.sh new file:   getData.js

---
+++ getData.js
@@ -0,0 +1,1 @@
+db.books.find({},{"volumeInfo.title":1, "volumeInfo.title":1,"volumeInfo.industryIdentifiers.type":1,"volumeInfo.industryIdentifiers.identifier":1 ,"volumeInfo.authors":1,"saleInfo.retailPrice":1,"saleInfo.isEbook":1,"volumeInfo.imageLinks.smallThumbnail":1,"accessInfo.publicDomain":1,"accessinfo.pdf.isAvailable":1,"_id":0})
