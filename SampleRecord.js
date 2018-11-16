82fd72fa73ff0f013982bb4fe7dfb3b55fd1f90f
Eamon Costello <eamon.costello@gmail.com>
2018-11-16 07:45:03 +0000

    Add files via upload

---
+++ SampleRecord.js
@@ -0,0 +1,34 @@
+{
+		"volumeInfo" : {
+			"title" : "Psychiatric and Mental Health Nursing",
+			"authors" : [
+				"Phil Barker"
+			],
+			"industryIdentifiers" : [
+				{
+					"type" : "ISBN_13",
+					"identifier" : "9781498759588"
+				},
+				{
+					"type" : "ISBN_10",
+					"identifier" : "1498759580"
+				}
+			],
+			"imageLinks" : {
+				"smallThumbnail" : "http://books.google.com/books/content?id=btSOCgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+			}
+		},
+		"saleInfo" : {
+			"isEbook" : true,
+			"retailPrice" : {
+				"amount" : 62.39,
+				"currencyCode" : "USD"
+			}
+		},
+		"accessInfo" : {
+			"publicDomain" : false,
+			"pdf" : {
+				"isAvailable" : true
+			}
+		}
+	}, }
